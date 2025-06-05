b68e7a1aba65bcc4744a775ab6f6a360f3e3e605
ujjwalmutneja2004 <124356243+ujjwalmutneja2004@users.noreply.github.com>
2025-06-05 11:19:41 +0530

    Update memory.js

--- store/memory.js
+++ store/memory.js
@@ -1,8 +1,4 @@
-// module.exports = {
-//   ingestionMap: new Map(), // ingestion_id -> { status, batches }
-//   batchMap: new Map(),     // batch_id -> { ids, status, ingestion_id }
-//   jobQueue: [],            // [{ priority, time, batch }]
-// };
+
 module.exports = {
   ingestionStore: {}, // ingestion_id -> { status, batches }
   batchQueue: [],     // [{ batch_id, ids, status, priority, ingestion_id, created_at }]
